7de2eca3451d6cb94593ad813f7bb3d6ed5747de
Pedro Vasconcellos <pedrovasconcellossp@gmail.com>
2021-03-02 03:26:49 -0300

    add Wait() Console App

--- Vasconcellos.FipeTable/Vasconcellos.FipeTable.ConsoleApp/Program.cs
+++ Vasconcellos.FipeTable/Vasconcellos.FipeTable.ConsoleApp/Program.cs
@@ -32,7 +32,7 @@ namespace Vasconcellos.FipeTable.ConsoleApp
             
             _logger.LogInformation("Starting Console FIPE TABLE.");
 
-            _uploadService.Process(false);
+            _uploadService.Process(false).Wait();
 
             _logger.LogInformation("Finalizing Console FIPE TABLE.");
             Console.ReadKey();
