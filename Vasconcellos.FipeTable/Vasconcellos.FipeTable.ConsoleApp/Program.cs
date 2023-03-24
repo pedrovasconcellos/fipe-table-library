8b72abbaf5c0268f61febe42087443e8f143fc98
Pedro Vasconcellos <pedrovasconcellossp@gmail.com>
2023-03-24 18:21:54 -0300

    add net7 and dotnetstandard2.0

--- Vasconcellos.FipeTable/Vasconcellos.FipeTable.ConsoleApp/Program.cs
+++ Vasconcellos.FipeTable/Vasconcellos.FipeTable.ConsoleApp/Program.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using System;
+﻿using System;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Vasconcellos.FipeTable.DownloadService.Infra;
 using Vasconcellos.FipeTable.DownloadService.Infra.Interfaces;
 using Vasconcellos.FipeTable.DownloadService.Services;
